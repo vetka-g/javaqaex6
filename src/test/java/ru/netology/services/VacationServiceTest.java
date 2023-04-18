3d851c26e9adf47e987cf8c9d2d0ec2b5ecee8b8
Sveta Golubeva <veta.golubeva@gmail.com>
2023-04-18 12:56:05 +0300

    Parametrized test commented

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -6,12 +6,31 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
-    @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/vacation.csv")
-    public void testVacationCount(int expected, int income, int expenses, int threshold) {
+    @Test
+    public void testVacationCountLess () {
         VacationService service = new VacationService();
-        int actual = service.calcVacation(income, expenses, threshold);
+
+        int expected = 3;
+        int actual = service.calcVacation(10000, 3000, 20000);
 
         Assertions.assertEquals(expected, actual);
     }
-}
+
+    @Test
+    public void testVacationCountMore () {
+        VacationService service = new VacationService();
+
+        int expected = 2;
+        int actual = service.calcVacation(100000, 60000, 150000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // @ParameterizedTest
+    // @CsvFileSource(files="src/test/resources/vacation.csv")
+    // public void testVacationCount(int expected, int income, int expenses, int threshold) {
+    // VacationService service = new VacationService();
+    // int actual = service.calcVacation(income, expenses, threshold);
+
+    // Assertions.assertEquals(expected, actual);
+    }
